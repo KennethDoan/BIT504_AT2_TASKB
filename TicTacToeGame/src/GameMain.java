e20a78671940684270b4f580343192ace8365046
KennethDoan <kennyd4321@gmail.com>
2021-05-08 17:55:31 +1200

    Setting up  JFrame

--- TicTacToeGame/src/GameMain.java
+++ TicTacToeGame/src/GameMain.java
@@ -1,10 +1,21 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
-public class GameMain {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("sdkds");
-
+public class GameMain extends JFrame
+{
+   
+	public GameMain() {
+		setTitle("Tic-Tac-Toe");
+		setSize(800,600);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
 	}
-
+    public static void main(String[] args) 
+    {
+      new GameMain();
+    }
 }
