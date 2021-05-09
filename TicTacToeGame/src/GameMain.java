fe154b4c5980505ea0ecebaf7cc31ce6031a1f76
KennethDoan <kennyd4321@gmail.com>
2021-05-09 14:33:45 +1200

    Setting up JFrame and setting InVOke Later Swing Utilities

--- TicTacToeGame/src/GameMain.java
+++ TicTacToeGame/src/GameMain.java
@@ -1,10 +1,30 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
-public class GameMain {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("sdkds");
-
+public class GameMain extends JFrame
+{
+	
+	private final static String WINDOW_TITLE ="Tic-Tac-Toe";
+	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
+   
+	public GameMain() {
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		setResizable(false);
+		add(new Board());
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
 	}
-
+    public static void main(String[] args) 
+    {
+    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+    	public void run() {
+    		new GameMain();
+    	}
+    });
+}
 }
